4f99e06d72854ad205ddfebee4126a7622ff1090
Ansuman-SG <95216613+Ansuman-SG@users.noreply.github.com>
2021-11-29 17:30:34 +0530

    Update App.java
    sefrew

--- DemoProject/src/main/java/com/abc/App.java
+++ DemoProject/src/main/java/com/abc/App.java
@@ -8,7 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to my Java World!" );
-	System.out.println( "Welcome to my New Pega World!" );
+       System.out.println( "Welcome to my Pega World!" );
     }
 }
